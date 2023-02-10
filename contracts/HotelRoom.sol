267ecceaff94d85c2062d9cf292853b30cd6bfd2
lciamp <l.ciamp@yahoo.com>
2023-02-10 08:40:00 -0500

    second

--- contracts/HotelRoom.sol
+++ contracts/HotelRoom.sol
@@ -1,33 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract HotellRoom {
-    // Eth payments
-    // modifiers
-    // visibility
-    // events
-    // enums
+// Showcase payment transfers, enums, modifiers & events
 
-    // vacant
-    // occupied
-    enum Statuses {Vacant, Occupied};
-    Statuses currentStatus;
+contract HotelRoom {
+    enum Statuses {
+        Vacant,
+        Occupied
+    }
+    Statuses public currentStatus;
+
+    event Occupy(address _occupant, uint256 _value);
 
     address payable public owner;
 
-    constructor(){
-        owner = msg.sender;
+    constructor() {
+        owner = payable(msg.sender);
         currentStatus = Statuses.Vacant;
     }
 
-    function book() payable {
-        // check price
-        // check status
-        require();
-        
+    modifier onlyWhileVacant() {
+        require(currentStatus == Statuses.Vacant, "Currently occupied.");
+        _;
+    }
+
+    modifier costs(uint256 _amount) {
+        require(msg.value >= _amount, "Not enough Ether provided.");
+        _;
+    }
+
+    function book() public payable onlyWhileVacant costs(2 ether) {
         currentStatus = Statuses.Occupied;
-        owner.transfer(msg.value);
 
+        (bool sent, bytes memory data) = owner.call{value: msg.value}("");
+        require(sent);
 
+        emit Occupy(msg.sender, msg.value);
     }
 }
---
+++ contracts/MyContract2.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Must be owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+}
+
+contract SecretVault {
+    string secret;
+
+    constructor(string memory _secret) {
+        secret = _secret;
+    }
+
+    function getSecret() public view returns(string memory){
+        return secret;
+    }
+}
+
+contract MyContract2 is Ownable {
+    // inheritance
+    // factories
+    // interaction
+    address secretVault;
+
+    constructor(string memory _secret) {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+        super;
+    }
+
+    function getSecret() public view onlyOwner returns(string memory){
+        return SecretVault(secretVault).getSecret();
+    }
+}
